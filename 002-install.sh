121f4b49e5266692b377fb5bd3ad4d8f2af35c21
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-07-07 16:56:30 +0200

    Update 002-install.sh

--- 002-install.sh
+++ 002-install.sh
@@ -15,5 +15,18 @@ arch_chroot() {
 
 # Copy files from Github
 arch_chroot "mkdir -p /mnt/mnt/etc/skel"
-arch_chroot "git clone https://github.com/bazeeel/baz-bspwm.git /mnt/mnt/etc/skel/"
+arch_chroot "git clone https://github.com/gabortomi/tom-bspwm.git /mnt/mnt/etc/skel/"
 arch_chroot "cp -rfT /mnt/mnt/etc/skel/ /etc/skel/"
+
+# Fstab
+genfstab -p /mnt >> /mnt/etc/fstab
+
+cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+
+echo "" >> /mnt/etc/pacman.conf;echo "[multilib]" >> /mnt/etc/pacman.conf;echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+arch_chroot "pacman -Syy"
+
+# Time Zone
+arch_chroot "ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime"
+arch_chroot "hwclock --systohc --utc"
+
