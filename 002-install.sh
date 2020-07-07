cbe2b8dd554d098f058db0409666ab8141ced259
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-07-07 16:49:14 +0200

    Update 002-install.sh

--- 002-install.sh
+++ 002-install.sh
@@ -8,3 +8,12 @@
 
 ###############################################################################
 
+# Command
+arch_chroot() {
+    arch-chroot /mnt /bin/bash -c "${1}"
+}
+
+# Copy files from Github
+arch_chroot "mkdir -p /mnt/mnt/etc/skel"
+arch_chroot "git clone https://github.com/bazeeel/baz-bspwm.git /mnt/mnt/etc/skel/"
+arch_chroot "cp -rfT /mnt/mnt/etc/skel/ /etc/skel/"
