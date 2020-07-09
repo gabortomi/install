0e99325aafd8893c136633e43e6962bebf249c09
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-07-09 10:22:37 +0200

    Update 002-install.sh

--- 002-install.sh
+++ 002-install.sh
@@ -15,6 +15,8 @@ arch_chroot() {
 
 user_name=tamas
 
+rootuuid=$(lsblk -lno UUID /dev/sda2)
+
 # Copy files from Github
 arch_chroot "mkdir -p /mnt/mnt/etc/skel"
 arch_chroot "git clone https://github.com/gabortomi/tom-bspwm.git /mnt/mnt/etc/skel/"
@@ -36,7 +38,8 @@ arch_chroot "hwclock --systohc --utc"
 echo "hu_HU.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 arch_chroot "locale-gen"
-export LANG=hu_HU.UTF-8
+arch_chroot "export LANG=hu_HU.UTF-8"
+arch_chroot "locale locale > /etc/locale.conf"
 
 echo "KEYMAP=hu"  > /mnt/etc/vconsole.conf
 
@@ -59,6 +62,9 @@ arch_chroot "passwd"
 # Boot loader
 pacstrap /mnt refind-efi efibootmgr
 arch_chroot "refind-install"
+echo "\"ArchBook Linux\" \"root=UUID=${rootuuid} rw add_efi_memmap\"" > /mnt/boot/refind_linux.conf
+echo "\"ArchBook Linux Fallback\" \"root=UUID=${rootuuid} rw add_efi_memmap initrd=/initramfs-linux-fallback.img\"" >> /mnt/boot/refind_linux.conf
+echo "\"ArchBook Linux Terminal\" \"root=UUID=${rootuuid} rw add_efi_memmap systemd.unit=multi-user.target\"" >> /mnt/boot/refind_linux.conf
 
 # Add a user
 arch_chroot "useradd -m -g users -G adm,lp,wheel,power,audio,video -s /bin/bash $user_name"
@@ -74,7 +80,7 @@ pacstrap /mnt xf86-video-intel libva-intel-driver lib32-mesa
 
 # Install desktop
 arch_chroot "yay -S --noconfirm --needed  xtitle-git sutils-git polybar dmenu2"
-arch_chroot "pacman -S --noconfirm --needed  bspwm sxhkd firefox firefox-i18n-hu alacritty picom dunst neovim pcmanfm zathura zathura-pdf-poppler zathura-ps zathura-djvu redshift"
+arch_chroot "pacman -S --noconfirm --needed  bspwm sxhkd firefox firefox-i18n-hu alacritty picom dunst neovim pcmanfm zathura zathura-pdf-poppler zathura-ps zathura-djvu redshift intel-ucode"
 arch_chroot "rm -rf /mnt/mnt"
 arch_chroot "cd /home/$user_name/; rm -rf .git/ LICENSE README.md git.sh setup-git.sh "
 
