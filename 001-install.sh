84d870946c96c05e2ccd6afa33948de16f705ab3
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-07-13 18:35:33 +0200

    Update 001-install.sh

--- 001-install.sh
+++ 001-install.sh
@@ -27,7 +27,7 @@ swapfile="yes"
 
 # Select the mirrors
 pacman -Sy --needed --noconfirm reflector
-reflector --verbose -l 50 -p https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
 # INstall the BASE and BASE-DEVEL packages
 pacstrap /mnt base base-devel linux linux-firmware git
