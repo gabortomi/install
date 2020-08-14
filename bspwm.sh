2bd0cf5210a6c7cd74aa7d440dc3d654d229718c
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-08-14 13:37:51 +0200

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -143,7 +143,7 @@ fi
 arch_chroot "xf86-video-intel libva-intel-driver lib32-mesa"
 
 # Install desktop
-arch_chroot "cd /home/${user_name} ; su ${user_name} -c ; yay -S --noconfirm --needed  xtitle-git sutils-git polybar dmenu2 "
+#arch_chroot "cd /home/${user_name} ; su ${user_name} -c ; yay -S --noconfirm --needed  xtitle-git sutils-git polybar dmenu2 "
 arch_chroot "pacman -S --noconfirm --needed  bspwm sxhkd firefox firefox-i18n-hu alacritty picom dunst neovim pcmanfm-gtk3 zathura zathura-pdf-poppler zathura-ps zathura-djvu redshift ttf-jetbrains-mono ttf-font-awesome discord rofi cronie polkit-gnome feh unclutter python-gobject reflector noto-fonts-emoji"
 arch_chroot "rm -rf /mnt/mnt"
 arch_chroot "cd /home/$user_name/; rm -rf .git/ LICENSE README.md git.sh setup-git.sh "
