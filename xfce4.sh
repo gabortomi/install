1eb25baa580ae39f3529ce7cf3137cd5a6162e29
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-08-30 15:45:50 +0200

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -8,6 +8,15 @@
 
 ###############################################################################
 
+# Command
+arch_chroot() {
+    arch-chroot /mnt /bin/bash -c "${1}"
+}
+
+user_name=tamas
+
+rootuuid=$(lsblk -lno UUID /dev/sda2)
+
 # UPDATE THE SYSTEM CLOCK
 timedatectl set-ntp true
 
@@ -31,27 +40,9 @@ swapfile="yes"
 pacman -Sy --needed --noconfirm reflector
 reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
-# INstall the BASE and BASE-DEVEL packages
-pacstrap /mnt base base-devel linux linux-firmware git
-
-#!/bin/bash
-
-#set -e
-
-###############################################################################
-
-# Author	:	Tamas Gabor
-
-###############################################################################
-
-# Command
-arch_chroot() {
-    arch-chroot /mnt /bin/bash -c "${1}"
-}
-
-user_name=tamas
-
-rootuuid=$(lsblk -lno UUID /dev/sda2)
+# Install the BASE and BASE-DEVEL packages
+    pacstrap /mnt base base-devel linux linux-firmware git
+    echo "end base"
 
 # Copy files from Github
 #arch_chroot "mkdir -p /mnt/mnt/etc/skel"
@@ -59,69 +50,54 @@ rootuuid=$(lsblk -lno UUID /dev/sda2)
 #arch_chroot "cp -rfT /mnt/mnt/etc/skel/ /etc/skel/"
 
 # Fstab
-genfstab -p /mnt >> /mnt/etc/fstab
+    genfstab -p /mnt >> /mnt/etc/fstab
 
-cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+    cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+    echo "" >> /mnt/etc/pacman.conf;echo "[multilib]" >> /mnt/etc/pacman.conf;echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+    
+    arch_chroot "pacman -Syy"
 
-echo "" >> /mnt/etc/pacman.conf;echo "[multilib]" >> /mnt/etc/pacman.conf;echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
-arch_chroot "pacman -Syy"
+    arch_chroot "passwd root"
 
-# Time Zone
-arch_chroot "ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime"
-arch_chroot "hwclock --systohc --utc"
+# Add a user
+    arch_chroot "useradd -m -g users -G adm,lp,wheel,power,audio,video -s /bin/bash $user_name"
+    echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
+    arch_chroot "passwd $user_name"
 
 # Locale
-echo "hu_HU.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-arch_chroot "locale-gen"
-export LANG=hu_HU.UTF-8
-locale > /mnt/etc/locale.conf
+    echo "hu_HU.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+    echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
+    arch_chroot "locale-gen"
+    export LANG=hu_HU.UTF-8
+    locale > /mnt/etc/locale.conf
 
 e#cho "KEYMAP=us"  > /mnt/etc/vconsole.conf
 
+# Time Zone
+    arch_chroot "ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime"
+    arch_chroot "hwclock --systohc --utc"
+    arch_chroot "timedatectl set-ntp true"
+
 # Hostname
-arch_chroot "echo archbook > /etc/hostname"
+    arch_chroot "echo archlinux > /etc/hostname"
 
 # Hosts
 # echo "127.0.0.1	localhost" >> /mnt/etc/hosts;echo "::1		localhost" >> /mnt/etc/hosts;echo "127.0.0.1	archbook.localdomain	archbook" >> /mnt/etc/hosts
 
 # Install basic apps (Xorg, Pulseaudio, ...)
-arch_chroot "pacman -S --noconfirm --needed xorg-server xorg-appres xorg-xinit networkmanager network-manager-applet xdg-user-dirs gvfs gvfs-mtp man-db neofetch xf86-video-fbdev bash-completion"
-arch_chroot "pacman -S --noconfirm --needed pulseaudio pulseaudio-alsa alsa-utils alsa-plugins alsa-firmware alsa-lib "
-arch_chroot "pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller"
-arch_chroot "systemctl enable NetworkManager"
+    arch_chroot "pacman -S --noconfirm --needed xorg-server xorg-appres xorg-xinit networkmanager network-manager-applet xdg-user-dirs gvfs gvfs-mtp man-db neofetch xf86-video-fbdev bash-completion"
+    arch_chroot "pacman -S --noconfirm --needed pulseaudio pulseaudio-alsa alsa-utils alsa-plugins alsa-firmware alsa-lib "
+    arch_chroot "pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller"
+    arch_chroot "systemctl enable NetworkManager"
 
-arch_chroot "pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter-settings lightdm-gtk-greeter"
-arch_chroot "systemctl enable lightdm.service -f"
-arch_chroot "systemctl set-default graphical.target"
-
-
-# Mkinitcpio
-arch_chroot "mkinitcpio -p linux"
-
-# Root passwd
-#arch_chroot "passwd"
-
-# Boot loader
-#pacstrap /mnt refind-efi efibootmgr
-#arch_chroot "refind-install"
-#echo "\"ArchBook Linux\" \"root=UUID=${rootuuid} rw add_efi_memmap\"" > /mnt/boot/refind_linux.conf
-#echo "\"ArchBook Linux Fallback\" \"root=UUID=${rootuuid} rw add_efi_memmap initrd=/initramfs-linux-fallback.img\"" >> /mnt/boot/refind_linux.conf
-#echo "\"ArchBook Linux Terminal\" \"root=UUID=${rootuuid} rw add_efi_memmap systemd.unit=multi-user.target\"" >> /mnt/boot/refind_linux.conf
-
-pacstrap /mnt grub efibootmgr
-arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot/efi"
-arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
-
-# Add a user
-arch_chroot "useradd -m -g users -G adm,lp,wheel,power,audio,video -s /bin/bash $user_name"
-echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
-#arch_chroot "passwd $user_name"
+    arch_chroot "pacman -S --noconfirm --needed lightdm lightdm-gtk-greeter-settings lightdm-gtk-greeter"
+    arch_chroot "systemctl enable lightdm.service -f"
+    arch_chroot "systemctl set-default graphical.target"
 
 # Yay
-arch_chroot "cd /home/${user_name} ; su ${user_name} -c 'git clone https://aur.archlinux.org/yay-bin' ; cd yay-bin ; su ${user_name} -c 'makepkg' ; pacman -U yay-bin*x86_64* --noconfirm ; cd .. ; rm -rf yay-bin"
+    arch_chroot "cd /home/${user_name} ; su ${user_name} -c 'git clone https://aur.archlinux.org/yay-bin' ; cd yay-bin ; su ${user_name} -c 'makepkg' ; pacman -U yay-bin*x86_64* --noconfirm ; cd .. ; rm -rf yay-bin"
 
-processor=$(lspci -n | awk -F " " '{print $2 $3}' | grep ^"06" | awk -F ":" '{print $2}' | sed -n  '1p')
+    processor=$(lspci -n | awk -F " " '{print $2 $3}' | grep ^"06" | awk -F ":" '{print $2}' | sed -n  '1p')
 
 if [ "$processor" = "8086" ]
 then
@@ -132,23 +108,31 @@ then
 fi
 
 # Install Intel VGA
-pacstrap /mnt xf86-video-intel libva-intel-driver lib32-mesa
+    pacstrap /mnt xf86-video-intel libva-intel-driver lib32-mesa
 
 # Install AMD VGA
-pacstrap /mnt xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-mesa lib32-libva-mesa-driver
-
+    pacstrap /mnt xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-mesa lib32-libva-mesa-driver
 
 # Install desktop
-arch_chroot "pacman -S --noconfirm --needed  xfce4 xfce4-goodies picom redshift discord polkit-gnome reflector noto-fonts-emoji"
-arch_chroot "rm -rf /mnt/mnt"
-arch_chroot "cd /home/$user_name/; rm -rf .git/ LICENSE README.md git.sh setup-git.sh "
+    arch_chroot "pacman -S --noconfirm --needed  xfce4 xfce4-goodies picom redshift discord polkit-gnome reflector noto-fonts-emoji"
+    arch_chroot "rm -rf /mnt/mnt"
+    arch_chroot "cd /home/$user_name/; rm -rf .git/ LICENSE README.md git.sh setup-git.sh "
 
-cd ..
+# Set makepkg.conf
+    sed -i 's/#MAKEFLAGS="-j[0-9]"/MAKEFLAGS="-j'$(nproc)'"/;s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T '$(nproc)' -z -)/;s/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -T'$(nproc)' -z -q -)/' /mnt/etc/makepkg.conf
+
+# Boot loader
+    pacstrap /mnt grub efibootmgr
+    arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot/efi"
+    arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 
 #git clone https://github.com/magyarchlinux/magyarch_xfce4.git 
 #mkdir -p /mnt/usr/share/backgrounds
 #cp -rf magyarch_xfce4/usr/share/backgrounds/magyarch/ /mnt/usr/share/backgrounds/
 
-git clone https://github.com/gabortomi/important-docs.git
-#cp -rf important-docs/30-touchpad.conf /mnt/etc/X11/xorg.conf.d/
-cp -rf important-docs/75-noto-color-emoji.conf /mnt/etc/fonts/conf.avail/
+cp -rf install/75-noto-color-emoji.conf /mnt/etc/fonts/conf.avail/
+
+# Mkinitcpio
+    arch_chroot "mkinitcpio -p linux"
+
+umount -R /mnt
