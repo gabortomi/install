e07c6369c34a0e20d928941bd95819415afc300c
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-12-05 17:19:58 +0100

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -89,8 +89,8 @@ reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 # echo "127.0.0.1	localhost" >> /mnt/etc/hosts;echo "::1		localhost" >> /mnt/etc/hosts;echo "127.0.0.1	archbook.localdomain	archbook" >> /mnt/etc/hosts
 
 # Install basic apps (Xorg, Pulseaudio, ...)
-    arch_chroot "pacman -S --noconfirm --needed xorg-server xorg-apps xorg-xinit xorg-twm xterm alsa-utils /
-    xorg-xbacklight pulseaudio pulseaudio-alsa xf86-input-libinput networkmanager xdg-user-dirs xdg-utils /
+    arch_chroot "pacman -S --noconfirm --needed xorg-server xorg-apps xorg-xinit xorg-twm xterm alsa-utils \
+    xorg-xbacklight pulseaudio pulseaudio-alsa xf86-input-libinput networkmanager xdg-user-dirs xdg-utils \
     gvfs gvfs-mtp man-db neofetch acpi xf86-video-fbdev bash-completion nm-connection-editor"
     
     #arch_chroot "pacman -S --noconfirm --needed unace unrar zip unzip sharutils uudeview arj cabextract file-roller"
@@ -116,11 +116,11 @@ fi
     #arch_chroot "xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-mesa lib32-libva-mesa-driver"
 
 # Install desktop
-    arch_chroot "pacman -S --noconfirm --needed  bspwm sxhkd xdo firefox firefox-i18n-hu alacritty picom dunst /
+    arch_chroot "pacman -S --noconfirm --needed  bspwm sxhkd xdo firefox firefox-i18n-hu alacritty picom dunst \
     pcmanfm-gtk3 polkit-gnome ttf-dejavu rofi"
-    arch_chroot "pacman -S --noconfirm --needed  picom ttf-jetbrains-mono firefox firefox-i18n-hu alacritty /
-    dunst neovim xorg-xsetroot lxappearance-gtk3 vifm discord unclutter unrar unzip rofi xorg-xbacklight /
-    polybar ttf-bitstream-vera ttf-dejavu ttf-font-awesome-4 ttf-jetbrains-mono ttf-joypixels unclutter /
+    arch_chroot "pacman -S --noconfirm --needed  picom ttf-jetbrains-mono firefox firefox-i18n-hu alacritty \
+    dunst neovim xorg-xsetroot lxappearance-gtk3 vifm discord unclutter unrar unzip rofi xorg-xbacklight \
+    polybar ttf-bitstream-vera ttf-dejavu ttf-font-awesome-4 ttf-jetbrains-mono ttf-joypixels unclutter \
     ttf-linux-libertine "
     
     #arch_chroot "pacman -S --noconfirm --needed     sutils-git xtitle-git  unrar unzip urlscan w3m xcape xclip xdotool xorg-xdpyinfo youtube-dl zathura zathura-pdf-poppler zathura-ps zathura-djvu mediainfo atool fzf highlight rofi xorg-xbacklight bc task-spooler polybar docx2txt odt2txt urxvt-perls rxvt-unicode pcmanfm"    
