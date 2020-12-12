17353b65c54c9a10156c03b808615d1f0baf86a7
gabortomi <60150731+gabortomi@users.noreply.github.com>
2020-12-12 13:24:06 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -21,11 +21,14 @@ timedatectl set-ntp true
 # Format the partition
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
 
 # Mount the filesystem
 mount /dev/sda2 /mnt
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mkdir -p /mnt/boot
+mkdir -p /mnt/home
+mount /dev/sda1 /mnt/boot/
+mount /dev/sda3 /mnt/home
 touch /mnt/swapfile
 
 dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
@@ -36,26 +39,29 @@ swapfile="yes"
 
 # Select the mirrors
 pacman -Sy --needed --noconfirm reflector
-reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist-arch
 
 # Install the BASE and BASE-DEVEL packages
-    pacstrap /mnt base base-devel linux linux-firmware
+    #pacstrap /mnt base base-devel linux linux-firmware
+    basestrap /mnt base base-devel runit elogind-runit linux linux-firmware
     echo "end base"
 
 # Fstab
-    genfstab -p /mnt >> /mnt/etc/fstab
-
-    cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+    #genfstab -p /mnt >> /mnt/etc/fstab
+    fstabgen -U /mnt >> /mnt/etc/fstab
+        
+    cp /etc/pacman.d/mirrorlist-arch /mnt/etc/pacman.d/mirrorlist-arch
     echo "" >> /mnt/etc/pacman.conf;echo "[multilib]" >> /mnt/etc/pacman.conf;echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+    echo "" >> /mnt/etc/pacman.conf;echo "[magyarch_repo]" >> /mnt/etc/pacman.conf;echo "SigLevel = Optional TrustedOnly" >> /mnt/etc/pacman.conf;echo 'Server = https://magyarchlinux.github.io/$repo/$arch' >> /mnt/etc/pacman.conf
     
     arch_chroot "pacman -Syy"
 
     arch_chroot "passwd root"
 
 # Add a user
-    arch_chroot "useradd -m -g users -G adm,lp,wheel,power,audio,video -s /bin/bash $user_name"
-    echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
-    arch_chroot "passwd $user_name"
+    #arch_chroot "useradd -m -g users -G adm,lp,wheel,power,audio,video -s /bin/bash $user_name"
+    #echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
+    #arch_chroot "passwd $user_name"
 
 # Locale
     echo "hu_HU.UTF-8 UTF-8" >> /mnt/etc/locale.gen
@@ -71,14 +77,14 @@ reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
 # Time Zone
     arch_chroot "ln -s /usr/share/zoneinfo/Europe/Budapest /etc/localtime"
-    arch_chroot "hwclock --systohc --utc"
+    arch_chroot "hwclock --systohc"
     arch_chroot "timedatectl set-ntp true"
 
 # Hostname
-    arch_chroot "echo archbook > /etc/hostname"
+    arch_chroot "echo laptop > /etc/hostname"
 
 # Hosts
-# echo "127.0.0.1	localhost" >> /mnt/etc/hosts;echo "::1		localhost" >> /mnt/etc/hosts;echo "127.0.0.1	archbook.localdomain	archbook" >> /mnt/etc/hosts
+    echo "127.0.0.1	localhost" >> /mnt/etc/hosts;echo "::1		localhost" >> /mnt/etc/hosts;echo "127.0.0.1	laptop.localdomain	laptop" >> /mnt/etc/hosts
 
 # Install basic apps (Xorg, Pulseaudio, ...)
     arch_chroot "pacman -S --noconfirm --needed networkmanager"
@@ -89,14 +95,14 @@ reflector --verbose -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
 if [ "$processor" = "8086" ]
 then
-    pacstrap /mnt intel-ucode
+    basestrap /mnt intel-ucode
 elif [ "$processor" = "1022" ]
 then
-    pacstrap /mnt amd-ucode
+    basestrap /mnt amd-ucode
 fi
 
 # Install Intel VGA
-    arch_chroot "pacman -S --noconfirm --needed xf86-video-intel libva-intel-driver lib32-mesa"
+    arch_chroot "pacman -S --noconfirm --needed xf86-video-intel intel-media-driver lib32-mesa"
 
 # Install AMD VGA
     #arch_chroot "xf86-video-amdgpu vulkan-radeon libva-mesa-driver lib32-mesa lib32-libva-mesa-driver"
@@ -106,16 +112,16 @@ fi
 
 # Boot loader
 
-    #pacstrap /mnt grub efibootmgr
-    #arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot/efi"
-    #arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
+    basestrap /mnt grub efibootmgr
+    arch_chroot "grub-install --target=x86_64-efi --efi-directory=/boot/efi"
+    arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 
-    pacstrap /mnt refind-efi efibootmgr
-    arch_chroot "refind-install"
-    rootuuid=$(lsblk -lno UUID /dev/sda2)
-    echo "\"Archbook\" \"root=UUID=${rootuuid} rw \"" > /mnt/boot/refind_linux.conf
-    echo "\"Archbook Fallback\" \"root=UUID=${rootuuid} rw initrd=/initramfs-linux-fallback.img\"" >> /mnt/boot/refind_linux.conf
-    echo "\"Archbook Terminal\" \"root=UUID=${rootuuid} rw systemd.unit=multi-user.target\"" >> /mnt/boot/refind_linux.conf
+    #pacstrap /mnt refind-efi efibootmgr
+    #arch_chroot "refind-install"
+    #rootuuid=$(lsblk -lno UUID /dev/sda2)
+    #echo "\"Archbook\" \"root=UUID=${rootuuid} rw \"" > /mnt/boot/refind_linux.conf
+    #echo "\"Archbook Fallback\" \"root=UUID=${rootuuid} rw initrd=/initramfs-linux-fallback.img\"" >> /mnt/boot/refind_linux.conf
+    #echo "\"Archbook Terminal\" \"root=UUID=${rootuuid} rw systemd.unit=multi-user.target\"" >> /mnt/boot/refind_linux.conf
 
 
 
@@ -138,7 +144,7 @@ fi
 
 cp -rf install/75-noto-color-emoji.conf /mnt/etc/fonts/conf.avail/
 
-arch_chroot "curl -LO https://github.com/gabortomi/LARBS/blob/master/larbs.sh && sh larbs.sh"
+arch_chroot "curl -LO github.com/gabortomi/LARBS/blob/master/larbs.sh && sh larbs.sh"
 
 
 umount -R /mnt
